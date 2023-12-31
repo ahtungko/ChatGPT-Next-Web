51aa417f407879a2f6ad6fdeac6d561b2ff8531e
ahtungko <itko1993@gmail.com>
2023-12-31 14:16:50 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { getServerSideConfig } from "./config/server";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "AI Assistant",
+  description: "Your personal AI Assistant.",
   viewport: {
     width: "device-width",
     initialScale: 1,
